0f31e7d21b48bfb4bceed44c1ccaa148aa748cc6
Jack McCown <jmccown@cbinsights.com>
2021-02-25 05:40:16 +0000

    api update jacks-dev-helpers.meta.js

--- jacks-dev-helpers.meta.js
+++ jacks-dev-helpers.meta.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         Jack's Dev Helpers
-// @version      0.1.0
+// @version      0.1.0.2021.2.25.0.39
 // @description  Hodgepodge of dev utils
+// @license      GNU GPLv3
 // @author       Jack McCown
 // @match        *://*/*
 // @namespace    jackmscripts
