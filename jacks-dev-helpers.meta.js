5586232ead201701119d7ac334329e2050b6930b
Jack McCown <jackmac79@gmail.com>
2021-09-02 18:17:24 +0000

    api update jacks-dev-helpers.meta.js

--- jacks-dev-helpers.meta.js
+++ jacks-dev-helpers.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Jack's Dev Helpers
-// @version      0.1.0.2021.9.2.17.20
+// @version      0.1.0.2021.9.2.18.16
 // @description  Hodgepodge of dev utils
 // @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @author       Jack McCown
