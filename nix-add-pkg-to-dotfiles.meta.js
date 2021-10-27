bcc89e6c2eeac28e75c42fe15fb67929bda30a56
gitlab autobot <jackmac79@gmail.com>
2021-10-27 00:43:17 +0000

    api update nix-add-pkg-to-dotfiles.meta.js

---
+++ nix-add-pkg-to-dotfiles.meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name       Nix Add Pkg To Dotfiles
+// @version    0.1.0.2021.10.27.0.42
+// @match      *://search.nixos.org/*
+// @namespace  jackmscripts
+// @run-at     document-end
+// ==/UserScript==
