d68b2f15625b0a6e30106ef42c9715eb671d6fc7
gitlab autobot <jackmac79@gmail.com>
2021-10-28 18:02:11 +0000

    api update

--- cbi-gitlab-helpers.meta.js
+++ cbi-gitlab-helpers.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       Cbi Gitlab Helpers
-// @version    0.1.0.2021.10.28.17.50
+// @version    0.1.0.2021.10.28.18.1
 // @match      *://gitlab.cbinsights.com/*
 // @license    GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @namespace  jackmscripts
