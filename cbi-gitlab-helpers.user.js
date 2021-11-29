a41f97d9c0e5f4243049c6ca4db1ae1b7e56d523
gitlab autobot <jackmac79@gmail.com>
2021-11-29 18:05:30 +0000

    api update

--- cbi-gitlab-helpers.user.js
+++ cbi-gitlab-helpers.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name       Cbi Gitlab Helpers
-// @version    0.1.0.2021.11.25.2.56
+// @version    0.1.0.2021.11.29.18.4
 // @match      *://gitlab.cbinsights.com/*
 // @license    GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @namespace  jackmscripts
 // @run-at     document-end
+// @updateURL  https://openuserjs.org/meta/jackm92/Cbi_Gitlab_Helpers.meta.js
 // ==/UserScript==
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
