42fca45ca5c038259308b06dd1371065772d0b51
gitlab autobot <jackmac79@gmail.com>
2022-01-04 02:58:43 +0000

    api update

--- nix-add-pkg-to-dotfiles.user.js
+++ nix-add-pkg-to-dotfiles.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name       Nix Add Pkg To Dotfiles
-// @version    0.1.0.2022.1.2.23.9
+// @version    0.1.0.2022.1.4.2.57
 // @match      *://search.nixos.org/*
 // @license    GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @namespace  jackmscripts
 // @run-at     document-end
+// @updateURL  https://openuserjs.org/meta/jackm92/Nix_Add_Pkg_To_Dotfiles.meta.js
 // ==/UserScript==
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
