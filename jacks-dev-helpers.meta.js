f5b87e3335d89245f1e7335b6b02ac947e2a11e1
gitlab autobot <jackmac79@gmail.com>
2024-10-27 03:11:03 +0000

    api update

--- jacks-dev-helpers.meta.js
+++ jacks-dev-helpers.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Jack's Dev Helpers
-// @version      0.1.0.2024.10.18.17.32
+// @version      0.1.0.2024.10.26.20.35
 // @description  Hodgepodge of dev utils
 // @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @author       Jack McCown
